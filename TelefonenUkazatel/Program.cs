3275832cf4ecff22e38b42c3f7b98e2e1566e550
SianaStoicheva <ss12712978@edu1.pgzaimov.com>
2024-05-16 14:31:31 +0300

    Zadachi

---
+++ TelefonenUkazatel/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace TelefonenUkazatel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                string name = tokens[1];
+
+                if (command == "A")
+                {
+                    string phoneNumber = tokens[2];
+                    phonebook[name] = phoneNumber;
+                }
+                else if (command == "S")
+                {
+                    if (phonebook.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {phonebook[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+            }
+        }
+    }
+}
